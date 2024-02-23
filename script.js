930245e10ddad26437052cd2256d5fc6b3644961
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-02-23 16:57:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,48 @@
 const makeChange = (num) => {
   // your name here
-	 let obj = {
-        0: ["Quarter", 25],
-        1: ["Dime", 10],
-        2: ["Nickel", 5],
-        3: ["Penny", 1],
-      };
-	
-	let arr = []
+	let obj = {
+          0: ["Quarter", 0.25],
+          1: ["Dime", 0.1],
+          2: ["Nickel", 0.05],
+          3: ["Penny", 0.01],
+        };
+        let num = c / 100;
+        console.log("hey" + num);
+        let arr = [];
         let quarter = 0,
           dime = 0,
           nickel = 0,
           penny = 0;
-        while (num > 0) {
+        while (num >= 0.01) {
           if (num >= obj[0][1]) {
-            num = num - obj[0][1];
+            num = num.toFixed(2) - obj[0][1];
+            num.toFixed(2);
             quarter++;
           }
           if (num >= obj[1][1] && num < obj[0][1]) {
-            num = num - obj[1][1];
+            num = num.toFixed(2) - obj[1][1];
+            num.toFixed(2);
             dime++;
           }
           if (num >= obj[2][1] && num < obj[1][1]) {
-            num = num - obj[2][1];
+            num = num.toFixed(2) - obj[2][1];
+            num.toFixed(2);
             nickel++;
           }
           if (num >= obj[3][1] && num < obj[2][1]) {
-            num = num - obj[3][1];
+            num = num.toFixed(2) - obj[3][1];
+            num.toFixed(2);
             penny++;
           }
         }
         arr.push(quarter, dime, nickel, penny);
         let q, d, n, p;
-        const obj1 = Object.assign(
-          { q: quarter, d: dime, n: nickel, p: penny}
-        );
+        const obj1 = Object.assign({
+          q: quarter,
+          d: dime,
+          n: nickel,
+          p: penny,
+        });
         return obj1;
 };
 
